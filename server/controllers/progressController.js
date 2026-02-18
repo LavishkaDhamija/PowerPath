5c1499edee2c1702ede0675ec8c967c0e643eeea
LavishkaDhamija <lavishkadhamija@gmail.com>
2026-02-18 23:40:27 +0530

    Created getProgress controller function

---
+++ server/controllers/progressController.js
@@ -0,0 +1,10 @@
+// @desc    Get student progress (accuracy, level, history)
+// @route   GET /api/progress
+// @access  Private
+const getProgress = async (req, res) => {
+    // Logic will go here
+};
+
+module.exports = {
+    getProgress
+};
